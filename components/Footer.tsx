910cc718f4b80cda6461ec7d5c5fd16c21567cc8
Justin Morrison <morrisonak@yahoo.com>
2022-12-08 21:36:52 -0600

    updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,9 +23,9 @@ export default function Footer() {
           <Link href="/">
             <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
           </Link>
-          <Link href="/about">
-            <a className="text-gray-500 hover:text-gray-600 transition">
-              About
+          <Link href="/text-gray-500 hover:text-gray-600 transition">
+              About">
+            <a className="
             </a>
           </Link>
           <Link href="/newsletter">
@@ -35,11 +35,11 @@ export default function Footer() {
           </Link>
         </div>
         <div className="flex flex-col space-y-4">
-          <ExternalLink href="https://twitter.com/leeerob">
+          <ExternalLink href="https://twitter.com/">
             Twitter
           </ExternalLink>
-          <ExternalLink href="https://github.com/leerob">GitHub</ExternalLink>
-          <ExternalLink href="https://www.youtube.com/channel/UCZMli3czZnd1uoc1ShTouQw">
+          <ExternalLink href="https://github.com/">GitHub</ExternalLink>
+          <ExternalLink href="https://www.youtube.com/channel/">
             YouTube
           </ExternalLink>
         </div>
@@ -57,7 +57,7 @@ export default function Footer() {
               Snippets
             </a>
           </Link>
-          <Link href="/tweets">
+          <Link href="/">
             <a className="text-gray-500 hover:text-gray-600 transition">
               Tweets
             </a>
